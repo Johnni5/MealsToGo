dd025b1c480ec5cf8355ef815a2c7d61ad92f439
Johnni5 <johnni5@proton.me>
2024-01-14 22:56:13 +0100

    "installed addons, changed version nr, added searchbar "

--- App.js
+++ App.js
@@ -1,17 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Platform,
+} from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
-  return;
-  <View style={styles.canvas}>
-    <Text>Our react native blank canvas</Text>
-  </View>;
+  const [searchQuery, setSearchQuery] = React.useState('');
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={setSearchQuery}
+            value={searchQuery}
+          />
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-  canvas: {
-    alignItems: "center",
-    backgroundColor: "blue",
-    color: "white",
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    backgroundColor: 'green',
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 16,
   },
 });
---
+++ App_old.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return;
+  <View style={styles.canvas}>
+    <Text>Our react native blank canvas</Text>
+  </View>;
+}
+
+const styles = StyleSheet.create({
+  canvas: {
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    color: 'white',
+  },
+});
